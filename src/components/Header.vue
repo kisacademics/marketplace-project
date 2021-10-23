640e3f1afa25550dc2ecc958e20aba0ccd7db47e
Josh Searle <joshryansearle@gmail.com>
2021-10-23 19:08:44 +1100

    Fix header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header
+  <!-- <header
     class="
       navigation-div
       w-full
@@ -31,6 +31,158 @@
       </nav>
     </div>
   </header>
+  <section
+    class="
+      sm:text-center
+      lg:text-left
+      sm:flex-row
+      flex-wrap
+      sm:py-16
+      px-4
+      sm:px-6
+      lg:px-8
+      xl:px-4
+      bg-default
+    "
+  >
+    <div class="max-w-6xl items-center flex sm:flex-row flex-col mx-auto">
+      <div class="sm:w-1/2 w-full sm:text-left text-center">
+        <p
+          class="text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl uppercase"
+        >
+          Kis Academics
+        </p>
+        <h1
+          class="
+            font-bold
+            text-white text-5xl
+            lg:text-7xl
+            hero-h1
+            home-hero-h1
+            sm:mb-0
+            mb-8
+          "
+        >
+          <span class="block">Find your</span>
+          <span class="block"><i class="relative">perfect</i> tutor</span>
+        </h1>
+      </div>
+      <div class="sm:w-1/2 w-full">
+        <div class="right-text sm:w-full w-72 mx-auto">
+          <ul
+            class="
+              ul-list
+              text-white
+              font-medium
+              xl:pl-10
+              green-check
+              text-left
+              sm:pl-14
+            "
+          >
+            <li class="sm:mb-8 mb-4 relative">Median ATAR of 99.50</li>
+            <li class="sm:mb-8 mb-4 relative">
+              Free study skills consultation
+            </li>
+            <li class="sm:mb-0 mb-10 relative">
+              Working with children's checks
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <section class="bg-default">
+    <div class="max-w-5xl w-3/4 mx-auto py-10 pb-12">
+      <!-- section with logo and title -->
+      <div
+        class="
+          flex
+          md:justify-between md:flex-row
+          flex-col
+          justify-center
+          items-center
+        "
+      >
+        <div>
+          <img
+            class="mx-auto logo-img w-min h-32 xl:h-40"
+            src="../assets/img/logo.png"
+          />
+        </div>
+
+        <div class="mt-8 md:mt-0">
+          <h1
+            class="
+              font-bold
+              text-white text-5xl
+              xl:text-7xl
+              hero-h1
+              home-hero-h1
+            "
+          >
+            <span class="block">Find your</span>
+            <span class="block"><i class="relative">perfect</i> tutor</span>
+          </h1>
+        </div>
+      </div>
+      <!-- Three ticks -->
+      <div
+        class="
+          mt-14
+          text-white
+          font-semibold
+          flex
+          lg:justify-between
+          flex-col
+          lg:flex-row
+          justify-center
+          mx-auto
+          w-72
+          lg:w-full
+        "
+      >
+        <div
+          v-for="message of [
+            'Median ATAR of 99.50',
+            'Free study skills consultation',
+            'Working with children\'s checks',
+          ]"
+          :key="message"
+          class="flex lg:mb-0 mb-4"
+        >
+          <div
+            class="
+              bg-green-500
+              rounded-full
+              w-6
+              h-6
+              flex
+              justify-center
+              text-black
+              items-center
+              mr-2
+            "
+          >
+            <svg
+              class="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <span>{{ message }}</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
